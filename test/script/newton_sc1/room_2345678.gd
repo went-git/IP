c3f1cbc08f0ff14c28ab1ad28c009a5c65052109
went-git <went.fals.2000@gmail.com>
2026-02-10 00:06:49 +0300

    сделан кабинет и коридор

---
+++ test/script/newton_sc1/room_2345678.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+@export var interaction_type: String = "dialog"  # "dialog" или "teleport"
+@export var dialog_text: String = "Тумбочка..а что в ней.?"
+@export var target_scene_path: String = ""
+
+
+func _ready():
+	# Добавляем в группу для фильтрации
+	add_to_group("interactable")
+	
+	# Соединяем сигналы
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node):
+	if body.is_in_group("player"):
+		body.set_interactable(self)
+
+func _on_body_exited(body: Node):
+	if body.is_in_group("player"):
+		body.clear_interactable(self)
+
+# Главный метод взаимодействия - будет переопределяться в дочерних классах
+func interact(player: CharacterBody2D) -> void:
+	var messages = PackedStringArray([
+		"...",
+		"а это вообще моя комната?"
+	])
+	player.show_labels(messages)	
+
+	#$Area2D/CollisionShape2D.disabled = true
